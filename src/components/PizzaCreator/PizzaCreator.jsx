49d8a192c435e43c9b60c1139932619bc1a4287d
Mr-innocence <bellali1019@gmail.com>
2020-02-06 18:38:34 +1100

    .

---
+++ src/components/PizzaCreator/PizzaCreator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PizzaCreator = () => {
+    <div class = "pizza-creator">
+        <div class="details">
+            Enter your details
+        </div>
+        <div class="size">
+            Select your size
+        </div>
+        <div class="toppings">
+            Choose your toppings
+        </div>
+        <div class="summary">
+            Summary
+        </div>
+        <button type="submit">Place order</button>
+    </div>
+}
